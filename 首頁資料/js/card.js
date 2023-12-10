64c1d10e38f0b5e0ee0be5959f6723008f893d19
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2023-12-10 17:29:13 +0800

    final-version

--- 首頁資料/js/card.js
+++ 首頁資料/js/card.js
@@ -1,117 +1,153 @@
 /*有空合併一下*/
 const myadd = document.getElementById("plus")
 const stone = document.getElementById("stone-num")
-var mygacha = [document.getElementById("gacha-1"),document.getElementById("gacha-10")]
+var mygacha = [document.getElementById("gacha-1"), document.getElementById("gacha-10")]
 const transition = document.getElementById("transition")
 const result = document.getElementById("result-area")
 const resultTxt = result.getElementsByTagName("h1")[0]
+const welcomebtn = document.getElementById("welcome-img")
+const welcomeAud = document.getElementById("welcome-audio")
+const welcomeVid = document.getElementById("welcome-video")
+const welcomelogo = document.getElementsByClassName("welcome-logo")
+const welcomeimg = document.getElementsByClassName("welcome-background")[0]
+const backgroundbtn = document.getElementById("change-background")
 const onecardVid = document.getElementById("one-card-video")
 const onecardAud = document.getElementById("one-card-audio")
 const skip = document.getElementById("skip")
 var cards = document.getElementsByClassName("card")
 
-var showlock = false
+var backgroundnum = 0
 var attempts = 0
 
 function fade(tar, speed, dir) {
     for (let i = 0; i < 1000; i++) {
-        setTimeout(()=>{tar.style.opacity = (dir?i:1000-i) / 1000;}, i * speed)
+        setTimeout(() => { tar.style.opacity = (dir ? i : 1000 - i) / 1000; }, i * speed)
     }
 }
 
-function slide(tar, speed,from,to){
+function slide(tar, speed, from, to) {
     for (let i = 0; i < 500; i++) {
-        setTimeout(()=>{
-                clearTimeout(); tar.style.left = String(from + ((Math.log10(i/50))*(to-from)) + "vw");
-            }, i * speed)
+        setTimeout(() => {
+            clearTimeout(); tar.style.left = String(from + ((Math.log10(i / 50)) * (to - from)) + "vw");
+        }, i * speed)
     }
 }
 
-function gacha(num){
-    if(stone.value<160*num){window.alert("no stone");return}
-
+function gacha(num) {
+    if (stone.value < 160 * num) { window.alert("no stone"); return }
     var pickCards = []
-    stone.value=parseInt(stone.value) - 160*num
-    let vid = transition.getElementsByTagName("video")[0]
-    for(let i = 0;i<num;i++){pickCards.push(pick())}
+    stone.value = parseInt(stone.value) - 160 * num
+    const vid = transition.getElementsByTagName("video")[0]
 
-    if(pickCards.includes(5) || pickCards.includes(6)){vid.src="首頁資料/卡池資訊/transition5.mp4"}
-    else if(pickCards.includes(4) || pickCards.includes(2)){vid.src="首頁資料/卡池資訊/transition4.mp4"}
-    else{vid.src="首頁資料/卡池資訊/transition3.mp4"}
+    for (let i = 0; i < num; i++) { pickCards.push(pick()) }
+    if (pickCards.includes(5) || pickCards.includes(6)) { vid.src = "首頁資料/卡池資訊/transition5.mp4" }
+    else if (pickCards.includes(4) || pickCards.includes(2)) { vid.src = "首頁資料/卡池資訊/transition4.mp4" }
+    else { vid.src = "首頁資料/卡池資訊/transition3.mp4" }
 
-    onecardAud.currentTime="0"
-    transition.style.display="initial"
-    onecardVid.style.display="none";
-    vid.currentTime = 0.1;vid.play();
-    onecardAud.currentTime="0";onecardAud.play()
-
-    showlock=false
-    if(num==10){
-        vid.addEventListener("ended",()=>{show_ten_cards(pickCards)},{once:true});
-        skip.addEventListener("click",()=>{show_ten_cards(pickCards)},{once:true});}
-    else{
-        vid.addEventListener("ended",()=>{show_card(pickCards)},{once:true});
-        skip.addEventListener("click",()=>{show_card(pickCards)},{once:true});}
-}
-
-function show_ten_cards(myCard){
-    let bestcard = [0,0]
-    if(showlock){return}
-    showlock = true;
-    onecardAud.pause()
-    transition.style.display="none";
-    result.style.display="initial";
-    for(let i = 0;i<cards.length;i++){
-        cards[i].src=""
-        setTimeout(()=>{
-            slide(cards[i],1,20,10);
-            if(bestcard[0]<myCard[i]){bestcard = [myCard,attempts+i+1]}
-
-            if(myCard[i]==6){cards[i].src="首頁資料/卡池資訊/5up.png"}
-            if(myCard[i]==2){cards[i].src="首頁資料/卡池資訊/4up" + parseInt(Math.random()*3) + ".png"}
-            if(myCard[i]==5){cards[i].src="首頁資料/卡池資訊/5norm" + parseInt(Math.random()*6) + ".png"}
-            if(myCard[i]==4){cards[i].src="首頁資料/卡池資訊/4norm" + parseInt(Math.random()*5) + ".png"}
-            if(myCard[i]==3){cards[i].src="首頁資料/卡池資訊/3.png"}
-            cards[i].style.opacity="1";
-        },i*100);
+    for (let i = 0; i < pickCards.length; i++) {
+        switch (pickCards[i]) {
+            case 6: { cards[i].src = "首頁資料/卡池資訊/5up.png"; break; }
+            case 2: { cards[i].src = "首頁資料/卡池資訊/4up" + parseInt(Math.random() * 3) + ".png"; break; }
+            case 5: { cards[i].src = "首頁資料/卡池資訊/5norm" + parseInt(Math.random() * 6) + ".png"; break; }
+            case 4: { cards[i].src = "首頁資料/卡池資訊/4norm" + parseInt(Math.random() * 5) + ".png"; break; }
+            case 3: { cards[i].src = "首頁資料/卡池資訊/3.png"; break; }
+        }
     }
-    setTimeout(()=>{
-        attempts+=10
-        result.addEventListener("click",()=>{
-            for(let c=0;c<cards.length;c++){
-                cards[c].style.opacity="0"
-            };
-            result.style.display="none";
-        },{once:true})
-    },2000)
-    
+
+    transition.style.display = "initial"
+    onecardVid.style.display = "none";
+    vid.currentTime = 0.1; vid.play();
+    onecardAud.currentTime = "0"; onecardAud.play()
+    const myfunc_ten = function () {
+        vid.removeEventListener('ended', myfunc_ten); skip.removeEventListener('click', myfunc_ten)
+        show_ten_cards();
+    }
+    const myfunc_one = function () {
+        vid.removeEventListener('ended', myfunc_one); skip.removeEventListener('click', myfunc_one)
+        show_card(pickCards);
+    }
+    if (num == 10) {
+        vid.addEventListener("ended", myfunc_ten);
+        skip.addEventListener("click", myfunc_ten);
+    }
+    else {
+        vid.addEventListener("ended", myfunc_one);
+        skip.addEventListener("click", myfunc_one);
+    }
 }
-function show_card(myCard){
-    if(showlock){return}
-    showlock = true
+
+function show_ten_cards() {
     onecardAud.pause()
+    transition.style.display = "none";
+    result.style.display = "initial";
+    for (let i = 0; i < cards.length; i++) {
+        setTimeout(() => {
+            slide(cards[i], 1, 20, 10);
+            cards[i].style.opacity = "1";
+        }, i * 100);
+    }
+    setTimeout(() => {
+        attempts += 10
+        result.addEventListener("click", () => {
+            for (let c = 0; c < cards.length; c++) {
+                cards[c].style.opacity = "0"
+            };
+            result.style.display = "none";
+        }, { once: true })
+    }, 2000)
 
-    for(let c=0;c<cards.length;c++){cards[c].style.opacity="0"};
-    resultTxt.style.opacity="1"
-    transition.style.display="none";
-    result.style.display="initial";
-    resultTxt.innerHTML=(myCard[0]==6?"5UP":myCard[0]==2?"4UP":myCard[0])+"星"
-
-    setTimeout(()=>{
-        if(myCard[0]==6){onecardVid.style.display="initial";resultTxt.style.opacity="0"}
-        onecardVid.style.opacity="1";onecardVid.currentTime="0"
-    },1)
-
-    setTimeout(()=>{result.addEventListener("click",()=>{
-        onecardVid.style.opacity="0";
-        resultTxt.style.opacity="0"
-        result.style.display="none";
-        attempts+=1
-    },{once:true})},1000)
 }
+function show_card(myCard) {
+    onecardAud.pause()
+    for (let c = 0; c < cards.length; c++) { cards[c].style.opacity = "0" };
+    resultTxt.style.opacity = "1"
+    transition.style.display = "none";
+    result.style.display = "initial";
+    resultTxt.innerHTML = (myCard[0] == 4 ? "4" : myCard[0] == 2 ? "4UP" : myCard[0]) + "星"
 
-window.onload=function(){
-    myadd.addEventListener("click",function(){stone.value=parseInt(stone.value)+1})
-    mygacha[0].addEventListener("click",function(){gacha(1)})
-    mygacha[1].addEventListener("click",function(){gacha(10)})
+    setTimeout(() => {
+        if (myCard[0] == 6 || myCard[0] == 3) {
+            resultTxt.style.opacity = "0"
+            onecardVid.style.display = "initial";
+            onecardVid.src = "首頁資料/卡池資訊/onecard" + myCard[0] + ".mp4"
+        }
+        onecardVid.style.opacity = "1"; onecardVid.currentTime = "0"
+    }, 1)
+
+    setTimeout(() => {
+        result.addEventListener("click", () => {
+            onecardVid.style.opacity = "0";
+            resultTxt.style.opacity = "0"
+            result.style.display = "none";
+            attempts += 1
+        }, { once: true })
+    }, 1000)
+}
+window.onload = function () {
+    myadd.addEventListener("click", function () { stone.value = parseInt(stone.value) + 1 })
+    mygacha[0].addEventListener("click", function () { gacha(1) })
+    mygacha[1].addEventListener("click", function () { gacha(10) })
+    welcomebtn.addEventListener('click',()=>{
+        welcomeVid.style.display='initial';
+        welcomebtn.style.display="none";
+        for(let i = 0;i<welcomelogo.length;i++){
+            welcomelogo[i].style.display="none"
+        }
+        document.getElementById("welcome-search").style.display="none"
+        backgroundbtn.style.display="none"
+        welcomeVid.play();
+        setTimeout(()=>{welcomeAud.play();},3000)
+        }
+    )
+    welcomeVid.addEventListener('ended',()=>{
+        welcomeVid.onclick=()=>{
+            fade(document.getElementsByClassName("welcome")[0],1,0);
+            setTimeout(()=>{document.getElementsByClassName("welcome")[0].style.display="none"},1000)
+        }
+        }
+    )
+    backgroundbtn.addEventListener("click",()=>{
+        backgroundnum<4?backgroundnum+=1:backgroundnum=0
+        welcomeimg.src="首頁資料/卡池資訊/background" + backgroundnum + ".jpg"
+    })
 }
--- 首頁資料/js/gacha.js
+++ 首頁資料/js/gacha.js
@@ -3,32 +3,32 @@ var FourStarCounter = 0
 var fiveUPrate = 0.5
 var fourUPrate = 0.5
 fivebasicrate = 0.009
-function setWeight(five,four){
-    w1 = Math.min(1,fivebasicrate + (five>73?(1-fivebasicrate)/17*(five-73):0)) 
-    w2 = Math.min(1,0.051 + (four>8?0.51*(four-8):0)) 
-    return [w1,w2]
+function setWeight(five, four) {
+    w1 = Math.min(1, fivebasicrate + (five > 73 ? (1 - fivebasicrate) / 17 * (five - 73) : 0))
+    w2 = Math.min(1, 0.051 + (four > 8 ? 0.51 * (four - 8) : 0))
+    return [w1, w2]
 }
-function pick(){
-    FiveStarCounter+=1
-    FourStarCounter+=1
-    let [FiveStarWeight, FourStarWeight ]= setWeight(FiveStarCounter,FourStarCounter)
-    ThreeStarWeight = 1-0.051-fivebasicrate
+function pick() {
+    FiveStarCounter += 1
+    FourStarCounter += 1
+    let [FiveStarWeight, FourStarWeight] = setWeight(FiveStarCounter, FourStarCounter)
+    ThreeStarWeight = 1 - 0.051 - fivebasicrate
     r = Math.random()
-    if(r<FiveStarWeight){
-        FiveStarCounter=0
-        if(Math.random()<=fiveUPrate){fiveUPrate=0.5;return 6}
-        fiveUPrate=1
+    if (r < FiveStarWeight) {
+        FiveStarCounter = 0
+        if (Math.random() <= fiveUPrate) { fiveUPrate = 0.5; return 6 }
+        fiveUPrate = 1
         return 5
-    }else if(r-FiveStarWeight<FourStarWeight){
-        FourStarCounter=0
-        if(Math.random()<=fourUPrate){fourUPrate=0.5;return 2}
-        fourUPrate=1
+    } else if (r - FiveStarWeight < FourStarWeight) {
+        FourStarCounter = 0
+        if (Math.random() <= fourUPrate) { fourUPrate = 0.5; return 2 }
+        fourUPrate = 1
         return 4
-    }else{
+    } else {
         return 3
     }
 }
-function reset(){
+function reset() {
     FiveStarCounter = 0
     FourStarCounter = 0
     fiveUPrate = 0.5
